d641ab378ebb221d7940a58386b86dc65fe023cf
Crispin Wellington <retrogradeorbit@gmail.com>
2016-04-09 12:09:05 +0800

    broke out line function and inline tests

--- src/diamond_kata/core.clj
+++ src/diamond_kata/core.clj
@@ -7,12 +7,31 @@
   [& args]
   (println "Hello, World!"))
 
+(defn- line [order num]
+  (case num
+    0 (str
+       (apply str (take (- order num) (repeat \space)))
+       (char (int \A))
+       )
+
+    ;; other line
+    nil
+    )
+)
+
+(comment
+
+  (= "A" (line 0 0))
+  (= " A" (line 1 0))
+
+)
+
 (defn diamond [order]
-  (str (char (int \A))))
+  (str (line order 0)))
 
 (deftest diamond-test
   (is (=
-       (diamond 1)
+       (diamond 0)
        "A"))
   (is (=
 
