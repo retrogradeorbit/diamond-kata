cb567c1002f5fae6d03672d689748fe7a759dd58
Crispin Wellington <retrogradeorbit@gmail.com>
2016-04-09 11:55:28 +0800

    make test pass and move test into mainline for quick REPL eval

--- src/diamond_kata/core.clj
+++ src/diamond_kata/core.clj
@@ -1,7 +1,17 @@
 (ns diamond-kata.core
+  (:require [clojure.test :refer :all])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn diamond [order]
+  (str (char (int \A))"\n"))
+
+(deftest diamond-test
+  (is (=
+       (diamond 1)
+       "A\n"))
+)
