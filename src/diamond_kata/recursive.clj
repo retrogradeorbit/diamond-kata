95587d93429f07fac6880eff6991c75801c5fd12
Crispin Wellington <retrogradeorbit@gmail.com>
2016-04-10 11:23:39 +0800

    recursive (not TCO) solution

---
+++ src/diamond_kata/recursive.clj
@@ -0,0 +1,32 @@
+(ns diamond-kata.recursive)
+
+(defn- char-for [num]
+  (char (+ (int \A) num)))
+
+(defn- n-spaces [n]
+  (apply str (take n (repeat \space))))
+
+(defn diamond-top [order]
+  (if (zero? order)
+    (vector (char-for 0))
+    (concat
+     (map
+      (partial str \space)
+      (diamond-top (dec order)))
+     [(str
+       (char-for order)
+       (n-spaces (dec (* 2 order)))
+       (char-for order))])))
+
+(defn diamond [order]
+  (apply
+   str
+   (interpose
+    \newline
+    (let [top (diamond-top order)]
+      (concat top (reverse (butlast top)))))))
+
+(defn -main
+  "Print out a diamond order n"
+  [n]
+  (println (diamond (read-string n))))
