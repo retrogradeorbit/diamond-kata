227b7e9088ae7105a572d52ca322d416fc1a06fc
Crispin Wellington <retrogradeorbit@gmail.com>
2016-10-15 20:23:31 +0800

    threading

--- src/diamond_kata/positional.clj
+++ src/diamond_kata/positional.clj
@@ -1,10 +1,9 @@
 (ns diamond-kata.positional)
 
-(defn- char-i [i]
-  (char (+ (int \A) i)))
-
 (defn- char-for-row [n y]
-  (char-i (if (<= y n) y (- (* 2 n) y))))
+  (-> (if (<= y n) y (- (* 2 n) y))
+      (+ (int \A))
+      char))
 
 (defn- char-at [n x y]
   (if (or (= n (+ x y))
@@ -20,7 +19,5 @@
                 (for [x s] (char-at n x y))
                 '(\newline)))))
 
-(defn -main
-  "Print out a diamond order n"
-  [n]
-  (println (flatten (diamond (read-string n)))))
+(defn -main [n]
+  (->> n read-string diamond flatten (apply str) print))
